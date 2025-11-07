ef9c344bf1630ce553b782d1dd0da5cb073e13f0
chiechie6999-code <chiechie6999@gmail.com>
2025-11-06 18:21:56 -0800

    Delete commit.sh

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-git checkout -b feat/add-ccmart-folder-final-5
-rm -rf ccmart
-mkdir ccmart
-touch ccmart/.gitkeep
-git add ccmart
-git commit -m "feat: Add empty ccmart directory"
-git status
